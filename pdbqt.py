6403fdfed13b1315571ee14e6366ada1acc0c005
Althea Hansel-Harris <ahansel@scripps.edu>
2022-05-10 17:49:45 -0700

    Made PDBQT dataclass and parser

---
+++ pdbqt.py
@@ -0,0 +1,80 @@
+from dataclasses import dataclass, field
+from parsers import pdbqt_parser
+import os
+import json
+
+
+@dataclass(frozen=True)
+class PDBQT:
+    name: str
+    sequence: str = field(default_factory=str, hash=False)
+    residues: set = field(default_factory=set, compare=False, hash=False)
+
+
+@dataclass(frozen=True)
+class Residue:
+    name: str
+    num: int
+    chain: str
+    atoms: set = field(default_factory=set, compare=False, hash=False)
+
+
+@dataclass(frozen=True)
+class Atom:
+    name: str
+    num: int
+    atomtype: str
+    atm_flag: str
+    x: float
+    y: float
+    z: float
+    occupancy: float
+    b_iso: float
+    q: float
+
+
+class PDBQTReader():
+
+    def __init__(self, fname):
+        self.fname = fname
+        self.name = fname.split(".")[0]
+
+    def make_pdbqt_object(self):
+        """Make PDBQT object from list of pdbqt line dictionaries
+
+        Args:
+            pdbqt_lines (list): list of dicitonaries, with each dictionary containing the data for one atom
+        """
+
+        pdbqt_lines = pdbqt_parser(self.fname)
+        pdbqt = PDBQT(self.name)
+
+        with open(os.path.join(os.path.dirname(__file__),
+                               'AA_CODES_3TO1.json'), 'r') as f:
+            aminoacid_codes_dict = json.load(f)
+
+        for atom in pdbqt_lines:
+            atom_name = atom["atm_name"]
+            atom_num = atom["atm_num"]
+            atomtype = atom["atomtype"]
+            atm_flag = atom["atm_flag"]
+            x = atom["x"]
+            y = atom["y"]
+            z = atom["z"]
+            occupancy = atom["occupancy"]
+            b_iso = atom["b_iso"]
+            q = atom["q"]
+            new_atom = Atom(atom_name, atom_num, atomtype, atm_flag, x, y, z, occupancy, b_iso, q)
+
+            resname = atom["res_name"]
+            resnum = atom["res_num"]
+            chain = atom["chain"]
+            current_res = Residue(resname, resnum, chain)
+            if current_res in pdbqt.residues:
+                pdbqt.residues[current_res].atoms.add(new_atom)
+            else:
+                current_res.atoms.add(new_atom)
+                pdbqt.residues.add(current_res)
+                pdbqt.sequence += aminoacid_codes_dict[current_res.name.replace(" ", "")]
+
+        return pdbqt
