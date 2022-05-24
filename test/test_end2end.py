7c55b00efbb0cef043a160a51ef9dc56df4a64e0
Althea Hansel-Harris <ahansel@scripps.edu>
2022-05-24 10:08:27 -0700

    Bug fixes from automated testing

--- test/test_end2end.py
+++ test/test_end2end.py
@@ -7,7 +7,6 @@
 import sqlite3
 import os
 import pytest
-
 """class TestInputs:
 	def test_multiple_files1(self):
 		os.system("python ../scripts/run_ringtail.py --file test_data/TEST_0000003-results/OB3Z3759450716_RX1--4xfx_mon_prep--tyr169.dlg.gz --file test_data/TEST_0000003-results/OB3Z3759440327_RX1--4xfx_mon_prep--tyr169.dlg.gz --file test_data/TEST_0000003-results/OB3Z3759457587_RX1--4xfx_mon_prep--tyr169.dlg.gz --file test_data/TEST_0000003-results/OB3Z3759444505_1_RX1--4xfx_mon_prep--tyr169.dlg.gz --file test_data/TEST_0000001-results/OB3Z3759305928_RX1--4xfx_mon_prep--tyr169.dlg.gz --no_print")
@@ -261,27 +260,230 @@ import pytest
 
 		os.system("rm output.db")
 
-		assert count == 1"""
+		assert count == 1
 
 
 class TestOutputs:
-	def test_export_table_csv(self):
-		status = os.system("python ../scripts/run_ringtail.py --file_list filelist1.txt --export_table_csv Ligands --no_print")
 
-		os.system("rm output.db")
+    def test_export_table_csv(self):
+        status = os.system(
+            "python ../scripts/run_ringtail.py --file_list filelist1.txt --export_table_csv Ligands --no_print"
+        )
 
-		assert status == 0
+        os.system("rm output.db")
 
-	def test_export_query_csv(self):
-		status = os.system("python ../scripts/run_ringtail.py --file_list filelist1.txt --export_query_csv 'SELECT * FROM Results' --no_print")
+        assert status == 0
 
-		os.system("rm output.db")
+    def test_export_query_csv(self):
+        status = os.system(
+            "python ../scripts/run_ringtail.py --file_list filelist1.txt --export_query_csv 'SELECT * FROM Results' --no_print"
+        )
 
-		assert status == 0
+        os.system("rm output.db")
+
+        assert status == 0
+
+    def test_interaction_tolerance(self):
+        status_notol = os.system(
+            "python ../scripts/run_ringtail.py --file_list filelist1.txt"
+        )
+
+        conn = sqlite3.connect("output.db")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Interaction_bitvectors WHERE Pose_ID in (SELECT Pose_ID FROM Results WHERE LigName LIKE 'OB3Z3759440327_RX1--4xfx_mon_prep--tyr169' AND run_number = 27)")
+        count_notol = sum([1 for interaction in cur.fetchone() if interaction == 1])
+
+        cur.close()
+        conn.close()
+
+        os.system("rm output.db")
+
+        status_tol = os.system(
+            "python ../scripts/run_ringtail.py --file_list filelist1.txt --interaction_tolerance"
+        )
+
+        conn = sqlite3.connect("output.db")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Interaction_bitvectors WHERE Pose_ID in (SELECT Pose_ID FROM Results WHERE LigName LIKE 'OB3Z3759440327_RX1--4xfx_mon_prep--tyr169' AND run_number = 27)")
+        count_tol = sum([1 for interaction in cur.fetchone() if interaction == 1])
+
+        cur.close()
+        conn.close()
+
+        os.system("rm output.db")
+
+        status_tol2 = os.system(
+            "python ../scripts/run_ringtail.py --file_list filelist1.txt --interaction_tolerance 2.0"
+        )
+
+        conn = sqlite3.connect("output.db")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Interaction_bitvectors WHERE Pose_ID in (SELECT Pose_ID FROM Results WHERE LigName LIKE 'OB3Z3759440327_RX1--4xfx_mon_prep--tyr169' AND run_number = 27)")
+        count_tol2 = sum([1 for interaction in cur.fetchone() if interaction == 1])
+
+        cur.close()
+        conn.close()
+
+        os.system("rm output.db")
+
+        assert status_notol == 0
+        assert status_tol == 0
+        assert status_tol2 == 0
+        assert count_notol != count_tol
+        assert count_tol2 != count_tol
+        assert count_tol2 != count_notol
+
+    def test_max_poses(self):
+        status3 = os.system(
+            "python ../scripts/run_ringtail.py --file_list filelist1.txt"
+        )
+        conn = sqlite3.connect("output.db")
+        cur = conn.cursor()
+        cur.execute("SELECT COUNT(*) FROM Results")
+        count3 = cur.fetchone()[0]
+
+        cur.close()
+        conn.close()
+
+        os.system("rm output.db")
+
+        status1 = os.system(
+            "python ../scripts/run_ringtail.py --file_list filelist1.txt --max_poses 1"
+        )
+        conn = sqlite3.connect("output.db")
+        cur = conn.cursor()
+        cur.execute("SELECT COUNT(*) FROM Results")
+        count1 = cur.fetchone()[0]
+
+        cur.execute("SELECT COUNT(*) FROM Ligands")
+        ligcount1 = cur.fetchone()[0]
+
+        cur.close()
+        conn.close()
+
+        os.system("rm output.db")
+
+        status5 = os.system(
+            "python ../scripts/run_ringtail.py --file_list filelist1.txt --max_poses 5"
+        )
+        conn = sqlite3.connect("output.db")
+        cur = conn.cursor()
+        cur.execute("SELECT COUNT(*) FROM Results")
+        count5 = cur.fetchone()[0]
+
+        cur.close()
+        conn.close()
+
+        os.system("rm output.db")
+
+        assert status1 == 0
+        assert status3 == 0
+        assert status5 == 0
+        assert count1 < count3 < count5
+        assert count1 == ligcount1
+
+    def test_store_all(self):
+        status = os.system(
+            "python ../scripts/run_ringtail.py --file_list filelist1.txt --store_all_poses"
+        )
+        conn = sqlite3.connect("output.db")
+        cur = conn.cursor()
+        cur.execute("SELECT COUNT(*) FROM Results")
+        count = cur.fetchone()[0]
+
+        cur.execute("SELECT COUNT(*) FROM Ligands")
+        ligcount = cur.fetchone()[0]
+
+        cur.close()
+        conn.close()
+
+        os.system("rm output.db")
+
+        assert status == 0
+        assert count == ligcount * 50"""
 
 
+class TestFilters():
+    def test_eworst(self):
+        status = os.system("python ../scripts/run_ringtail.py --file_list filelist1.txt --eworst -15")
 
+        os.system("rm output.db")
 
+        assert status == 0
 
+    def test_ebest(self):
+        status = os.system("python ../scripts/run_ringtail.py --file_list filelist1.txt --ebest -15")
 
+        os.system("rm output.db")
 
+        assert status == 0
+
+    def test_leworst(self):
+        status = os.system("python ../scripts/run_ringtail.py --file_list filelist1.txt --leworst -0.4")
+
+        os.system("rm output.db")
+
+        assert status == 0
+
+    def test_lebest(self):
+        status = os.system("python ../scripts/run_ringtail.py --file_list filelist1.txt --leworst -0.4")
+
+        os.system("rm output.db")
+
+        assert status == 0
+
+    def test_epercentile(self):
+        status = os.system("python ../scripts/run_ringtail.py --file_list filelist1.txt --epercentile 0.1")
+
+        os.system("rm output.db")
+
+        assert status == 0
+
+    def test_lepercentile(self):
+        status = os.system("python ../scripts/run_ringtail.py --file_list filelist1.txt --leffpercentile 0.1")
+
+        os.system("rm output.db")
+
+        assert status == 0
+
+    def test_name(self):
+        status = os.system("python ../scripts/run_ringtail.py --file_list filelist1.txt --name OB3Z3759440327_RX1--4xfx_mon_prep--tyr169")
+
+        os.system("rm output.db")
+
+        assert status == 0
+
+    def test_substruct(self):
+        status = os.system("python ../scripts/run_ringtail.py --file_list filelist1.txt --substructure CO")
+
+        os.system("rm output.db")
+
+        assert status == 0
+
+    def test_substruct_join_or(self):
+        status = os.system("python ../scripts/run_ringtail.py --file_list filelist1.txt --substructure CO --substructure OC")
+
+        os.system("rm output.db")
+
+        assert status == 0
+
+    def test_substruct_join_and(self):
+        status = os.system("python ../scripts/run_ringtail.py --file_list filelist1.txt --substructure CO --substructure C=C --substruct_join AND")
+
+        os.system("rm output.db")
+
+        assert status == 0
+
+    def test_hbcount(self):
+        status = os.system("python ../scripts/run_ringtail.py --file_list filelist1.txt --hb_count 5")
+
+        os.system("rm output.db")
+
+        assert status == 0
+
+    def test_react_any(self):
+        status = os.system("python ../scripts/run_ringtail.py --file_list filelist1.txt --react_any")
+
+        os.system("rm output.db")
+
+        assert status == 0
