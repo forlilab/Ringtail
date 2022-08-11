f54f29e0bfad07bddcf4cc86869ea60183e94d58
Althea Hansel-Harris <ahansel@scripps.edu>
2022-08-11 15:21:00 -0700

    Remake bookmarks in exported database

--- ringtail/vsmanager.py
+++ ringtail/vsmanager.py
@@ -390,6 +390,7 @@ class VSManager:
         # connect to cloned database
         db_clone = DBManagerSQLite(bookmark_db_name, self.db_opts)
         db_clone.prune()
+        db_clone.remake_bookmarks()
         db_clone.close_connection(vacuum=True)
 
     def close_database(self):
