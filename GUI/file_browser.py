d1c249cf40492f05329cd93cb0ecfa94cd5f29b7
maylinnp <paulsen.maylinn@gmail.com>
2024-07-12 08:57:39 -0700

    cleaned up some methods and added doc strings

--- GUI/file_browser.py
+++ GUI/file_browser.py
@@ -6,12 +6,25 @@ from PyQt5.uic import loadUi
 
 class FileBrowser(QDialog):
     def __init__(self, parent):
+        """
+        Initialize the UI from the .ui file, and assign button click values etc
+
+        Args:
+            parent (object): Main window that the browser interacts with (to and from)
+        """
         super(FileBrowser, self).__init__()
         loadUi("GUI/file_dialog.ui", self)
+
+        # disable/enable control buttons
+        self.close_filebrowser_window_button.setEnabled(True)
+        self.submit_files_button.setEnabled(False)
+
+        # connect buttons to browing results
         self.browse_files_button.clicked.connect(self.browsefiles)
         self.browse_directories_button.clicked.connect(self.browsedirectories)
         self.browse_filelists_button.clicked.connect(self.browsefilelists)
-        self.submit_files_button.clicked.connect(self.submit_files)
+
+        # connect buttons to deleting selected paths
         self.delete_file_button.clicked.connect(
             lambda: self.delete_item(self.file_list)
         )
@@ -22,9 +35,19 @@ class FileBrowser(QDialog):
             lambda: self.delete_item(self.filelist_list)
         )
 
+        # submit files or close window
+        self.submit_files_button.clicked.connect(self.submit_files)
+        self.close_filebrowser_window_button.clicked.connect(self.close)
+
         self.parent = parent
 
     def selectfiles(self, extension: str = ""):
+        """
+        Method that will allow finding files only with the given extension, and select one or more
+
+        Args:
+            extension (str): File extension that is allowed to search for
+        """
         filenames, _ = QFileDialog.getOpenFileNames(
             self,
             "QFileDialog.getOpenFileNames()",
@@ -37,13 +60,21 @@ class FileBrowser(QDialog):
                 self.filelist_list.insertItems(0, filenames)
             else:
                 self.file_list.insertItems(0, filenames)
-            self.file_browser_text.setText(filenames[0])
+        # enable submit button only if there are files
+        if len(self.filelist_list) > 0 or len(self.file_list) > 0:
+            self.submit_files_button.setEnabled(True)
 
     def browsefiles(self):
-        # this should be set by the chosen docking mode
+        """
+        Browse results files with file extension given from main UI
+
+        """
         return self.selectfiles(self.parent.file_pattern())
 
     def browsedirectories(self):
+        """
+        Browses folders and can pick one or more
+        """
         dialog = QtWidgets.QFileDialog(self)
         dialog.setWindowTitle("Choose Directories")
         dialog.setOption(QtWidgets.QFileDialog.DontUseNativeDialog, True)
@@ -54,13 +85,24 @@ class FileBrowser(QDialog):
 
         if dialog.exec_() == QtWidgets.QDialog.Accepted:
             self.directory_list.insertItems(0, dialog.selectedFiles())
-            self.file_browser_text.setText(dialog.selectedFiles()[0])
         dialog.deleteLater()
 
+        if len(self.directory_list) > 0:
+            self.submit_files_button.setEnabled(True)
+
     def browsefilelists(self):
-        self.selectfiles("*.txt")
+        """
+        Browses for file lists, which is any file with extension "*.txt"
+        """
+        return self.selectfiles("*.txt")
 
     def delete_item(self, list_widget):
+        """
+        Method to remove items from a QListWidget that are marked in the list
+
+        Args:
+            list_widget (QListWidget): List widget from which to take items off
+        """
         # get index of the items, and remove from bottom up so index doesn't change
         indices = [x.row() for x in list_widget.selectedIndexes()]
         # then remove each item from bottom up (so indices don't change)
@@ -68,42 +110,41 @@ class FileBrowser(QDialog):
             list_widget.takeItem(index)
 
     def submit_files(self):
+        """
+        Submit files to the main window if files are prsent, and updates the text boxes showing how many items were collected in each category
+        """
         files = False
-        self.parent.num_of_directories_display.setText(str(len(self.directory_list)))
-
-        self.parent.num_of_files_display.setText(str(len(self.file_list)))
-
-        self.parent.num_of_filelists_display.setText(str(len(self.filelist_list)))
+        # TODO I can def remove some redundancies here, maybe use lambda expressions or other
+        num_dir = str(len(self.directory_list))
+        num_fl = str(len(self.file_list))
+        num_fll = str(len(self.filelist_list))
+        self.parent.num_of_directories_display.setText(num_dir)
+        self.parent.num_of_files_display.setText(num_fl)
+        self.parent.num_of_filelists_display.setText(num_fll)
 
         if len(self.file_list) > 0:
-            self.parent.files = [
-                str(self.file_list.item(i).text())
-                for i in range(self.file_list.count())
-            ]
+            self.parent.files = self.file_list
             files = True
-            print(self.parent.files)
 
         if len(self.filelist_list) > 0:
-            self.parent.filelists = [
-                str(self.filelist_list.item(i).text())
-                for i in range(self.filelist_list.count())
-            ]
+            self.parent.filelists = self.filelist_list
             files = True
-            print(self.parent.filelists)
 
         if len(self.directory_list) > 0:
-            self.parent.directories = [
-                str(self.directory_list.item(i).text())
-                for i in range(self.directory_list.count())
-            ]
+            self.parent.directories = self.directory_list
             files = True
-            print(self.parent.directories)
 
         if files:
             self.parent.submit_files_button.setEnabled(True)
         else:
             self.parent.submit_files_button.setEnabled(False)
 
+        self.close_window()
+
+    def close_window(self):
+        """
+        Close the file browser
+        """
         self.close()
 
 
