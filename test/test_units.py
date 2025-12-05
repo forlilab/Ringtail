848ed73d243eb47c8a11ec6a9eaaec5d8d95ebe5
maylinnp <paulsen.maylinn@gmail.com>
2025-12-05 12:41:36 -0800

    make cluster tests independent of one another

--- test/test_units.py
+++ test/test_units.py
@@ -204,7 +204,7 @@ class TestRingtailCore:
 
     def test_similar_ligands_interaction(self, monkeypatch):
 
-        rtc = RingtailCore(db_file="cluster.db")
+        rtc = RingtailCore(db_file="ifp_cluster.db")
         rtc.add_results_from_files(
             file_path=["test_data/adgpu/group1/", "test_data/adgpu/group2/"],
         )
@@ -212,16 +212,20 @@ class TestRingtailCore:
         rtc.filter(ebest=-6, interaction_cluster=0.5)
         monkeypatch.setattr("builtins.input", lambda _: 1)  # provides terminal input
         number_similar = rtc.find_similar_ligands(ligand_name)
+        os.system("rm ifp_cluster.db")
 
         assert number_similar == 13
 
     def test_similar_ligands_mfpt(self, monkeypatch):
-        rtc = RingtailCore(db_file="cluster.db")
+        rtc = RingtailCore(db_file="mfpt_cluster.db")
+        rtc.add_results_from_files(
+            file_path=["test_data/adgpu/group1/", "test_data/adgpu/group2/"],
+        )
         ligand_name = "287065"
         rtc.filter(ebest=-6, mfpt_cluster=0.5)
-        monkeypatch.setattr("builtins.input", lambda _: 2)  # provides terminal input
+        monkeypatch.setattr("builtins.input", lambda _: 1)  # provides terminal input
         number_similar = rtc.find_similar_ligands(ligand_name)
-        os.system("rm cluster.db")
+        os.system("rm mfpt_cluster.db")
         assert number_similar == 8
 
     def test_create_rdkitmol(self):
