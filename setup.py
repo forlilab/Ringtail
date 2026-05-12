2aca4a06d28eeac85dbc2045f118af1479033bf7
maylinnp <paulsen.maylinn@gmail.com>
2026-05-12 13:23:54 -0700

    add db version comp

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def find_files(directory):
 
 setup(
     name="ringtail",
-    version="2.3",
+    version="2.3.0",
     author="Forli Lab",
     author_email="forli@scripps.edu",
     url="https://github.com/forlilab/Ringtail",
